9d77a0bdeb0c649beeb8de9929e784389d4340d1
rcasto <rcasto92@gmail.com>
2015-06-02 20:19:43 -0700

    edit startup logging

--- bin/start.js
+++ bin/start.js
@@ -2,6 +2,8 @@ var server = require('../server');
 
 var port = process.env.PORT || 3000;
 
+console.log("Using port: " + 3000);
+
 server.listen(port, function () {
 	console.log("Server started on port: " + port);
 });
