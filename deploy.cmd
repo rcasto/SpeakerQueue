3c602d3376a01aae77237c55f9c687a0ea5716e5
Richie Casto <Richie Casto>
2015-12-19 14:46:53 -0800

    See if this fixes deployment

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,6 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install
-  call :ExecuteCmd !NPM_CMD! start
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
